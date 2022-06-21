6e3c0456c51c1e773f704ecd79b6fdc8be20304f
duha ciftci <93863007+dhciftci@users.noreply.github.com>
2022-06-22 01:15:51 +0300

    Add files via upload

--- mean.js
+++ mean.js
@@ -1,34 +1,9 @@
-let job = "manager";
-let salary = 0;
-switch (job) {
-  case "manager":
-    salary = 8000;
-    console.log(`salary is ${salary}`);
-    break;
-  case "it":
-  case "support":
-    salary = 6000;
-    console.log(`salary is ${salary}`);
-    break;
-  case "developer":
-  case "degisner":
-    salary = 7000;
-    console.log(`salary is ${salary}`);
-  default:
-    salary = 4000;
-    console.log(`salary is ${salary}`);
-}
+let zero = 0;
+let counter = 3;
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
 
-let holdiday = 0;
-let money = 0;
-if (holdiday === 0) {
-  money = 5000;
-  console.log(`my money is${money}`);
-} else if (holdiday === 1 || holdiday === 2) {
-  money = 3000;
-  console.log(`my money is${money}`);
-} else if (holdiday === 3);
-{
-  money = 2000;
-  console.log(`my money is${money}`);
-}
+my.splice(zero, my.length, '"Elzero"');
+console.log(my.join() + my.split(""));
+console.log(my.slice(my.indexOf("Mazero"), counter));
+
+console.log(my.reverse());
